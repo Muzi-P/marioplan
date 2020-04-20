78fc2d92d77c741ed11dfa8c6219ca24e396bc20
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2020-04-20 11:05:58 +0800

    Formatting Data with moments

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment'
 
 function ProjectSummary({ project }) {
     return (
@@ -8,7 +9,7 @@ function ProjectSummary({ project }) {
                     {project.title}
                 </span>
                 <p>{`Posted by ${project.authorFirstName} ${project.authorLastName}`}</p>
-                <p className="grey-text">3rd September, 2am</p>
+                <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
