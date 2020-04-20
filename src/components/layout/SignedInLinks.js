6e734fc49615d37a50ae8c5d9df07daf690e5865
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2020-04-20 14:56:57 +0800

    Cloud Function Auth Trigger

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -8,8 +8,8 @@ const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><a onClick={props.signOut} href="true">Log Out</a></li>
-    <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{props.profile.initials}</NavLink></li>
+            <li><a onClick={props.signOut} href="/">Log Out</a></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{props.profile.initials}</NavLink></li>
         </ul>
     )
 }
