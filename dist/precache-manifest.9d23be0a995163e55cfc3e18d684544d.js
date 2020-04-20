5307673edd3156a88a5e30040d04e4f261d400a2
Muzi P. Gondwe <phiwokuhlegondwe@gmail.com>
2020-04-20 15:40:46 +0800

    Deploying to Firebase Hosting

---
+++ dist/precache-manifest.9d23be0a995163e55cfc3e18d684544d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baf7e09429c7bad067fc8789373f216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7feef72694b623b4d677",
+    "url": "/static/css/main.2368f16c.chunk.css"
+  },
+  {
+    "revision": "1e1c91b2a88569921bb5",
+    "url": "/static/js/2.81ce4451.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.81ce4451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7feef72694b623b4d677",
+    "url": "/static/js/main.b18574a4.chunk.js"
+  },
+  {
+    "revision": "381242720deef3aa2fcc",
+    "url": "/static/js/runtime-main.6dfdfc02.js"
+  }
+]);
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,9 @@ import { Redirect } from 'react-router-dom';
 
 export class Dashboard extends Component {
     render() {
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
+        console.log(notifications);
+
         if (!auth.uid) return <Redirect to='/signin' />
         return (
             <div className="dashboard container">
@@ -17,7 +19,7 @@ export class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -28,7 +30,8 @@ export class Dashboard extends Component {
 const mapStateToProps = (state) => {
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
@@ -36,6 +39,7 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
     ])
 )(Dashboard)
